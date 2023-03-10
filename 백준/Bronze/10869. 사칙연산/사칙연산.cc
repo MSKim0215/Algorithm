463deb5362532ed944f8c3f4876a0a0aac7e755b
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-03-10 17:06:09 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int inputA, inputB;
+    
+    cin >> inputA;
+    cin >> inputB;
+    
+    cout << inputA + inputB << endl;
+    cout << inputA - inputB << endl;
+    cout << inputA * inputB << endl;
+    cout << inputA / inputB << endl;
+    cout << inputA % inputB << endl;
+}
