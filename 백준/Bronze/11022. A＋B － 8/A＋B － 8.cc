fd933a6d23e31793e418bad4076b7c97f5ebff25
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-03-11 22:38:27 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int T;
+    cin >> T;
+    
+    for(int i = 1; i <= T; i++)
+    {
+        int A, B;
+        cin >> A >> B;
+        cout << "Case #" << i << ": " << A << " + " << B << " = " << A+B << "\n";
+    }
+}
