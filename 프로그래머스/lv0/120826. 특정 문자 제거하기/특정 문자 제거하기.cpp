969369a2f1701a677696fcdb10128f3cc09244c0
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-06-07 14:20:07 +0900

    Update 특정 문자 제거하기.cpp

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -6,15 +6,13 @@ using namespace std;
 // my_string: 주어진 문자열, letter: 필터링 문자열
 string solution(string my_string, string letter) {
     string answer;
+    char filter = letter[0];        // letter은 항상 길이가 1인 알파벳
     
     for(int i = 0 ; i < my_string.length(); i++)
     {
-        for(int j = 0; j < letter.length(); j++)
+        if(my_string[i] != filter)
         {
-            if(my_string[i] != letter[j])
-            {
-                answer += my_string[i];
-            }
+            answer += my_string[i];
         }
     }
     
