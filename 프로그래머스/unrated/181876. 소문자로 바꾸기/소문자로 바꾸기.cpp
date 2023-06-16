9059b6b4bcfb01ab53577abc76fddc1037353aa0
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-06-16 10:53:32 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for(int i = 0; i < myString.length(); i++)
+    {
+        if(myString[i] >= 'A' && myString[i] <= 'Z')
+        {
+            myString[i] += 32;
+        }
+    }
+    return myString;
+}
