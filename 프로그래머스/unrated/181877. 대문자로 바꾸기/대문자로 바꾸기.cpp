2fb45d3611833bceb6eadf49f44d3017546d9fbd
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-06-21 10:50:28 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    for(int i = 0; i < myString.length(); i++)
+    {
+        if(myString[i] >= 'a' && myString[i] <= 'z')
+        {
+            answer += (myString[i] - 32);
+        }
+        else
+        {
+            answer += myString[i];
+        }
+    }
+    
+    return answer;
+}
