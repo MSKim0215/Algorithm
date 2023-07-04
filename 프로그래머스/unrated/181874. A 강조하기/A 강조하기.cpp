ad0d86374c7f30ea24b394ea32904d5a07679843
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-07-04 09:43:23 +0900

    [unrated] Title: A 강조하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for(int i = 0; i < myString.length(); i++)
+    {
+        if(myString[i] == 'a') myString[i] = 'A';
+        else if(myString[i] >= 'B' && myString[i] <= 'Z')
+        {
+            myString[i] += 32;
+        }
+    }
+    return myString;
+}
