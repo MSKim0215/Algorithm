28d976005ac3a9091b6a7d34e4aa613209a249bc
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-07-11 11:25:48 +0900

    [unrated] Title: l로 만들기, Time: 0.36 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {  
+    for(int i = 0; i < myString.length(); i++)
+    {
+        if(myString[i] >= 'a' && myString[i] < 'l')
+        {
+            myString[i] = 'l';
+        }
+    }
+    
+    
+    return myString;
+}
