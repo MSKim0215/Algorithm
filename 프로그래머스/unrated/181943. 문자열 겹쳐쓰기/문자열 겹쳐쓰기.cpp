ef323e6611a8623c04c728cdffb129792f955cbe
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-07-27 11:47:32 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    
+    for(int i = 0; i < s; i++)
+    {
+        answer += my_string[i];
+    }
+    
+    for(int i = 0; i < overwrite_string.length(); i++)
+    {
+        answer += overwrite_string[i];
+    }
+    
+    if(answer.length() < my_string.length())
+    {
+        for(int i = overwrite_string.length() + s; i < my_string.length(); i++)
+        {
+            answer += my_string[i];
+        }
+    }
+    
+    return answer;
+}
