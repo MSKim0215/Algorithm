a6624a82d9b910d265f9dbc6d0e1cf29062db75e
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-12-05 10:48:21 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.08 ms, Memory: 3.92 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<int> query)
+{
+    for(int i = 0; i < query.size(); i++)
+    {
+        vector<int> temp;
+        
+        if(i % 2 == 0)
+        {   // 짝수
+            for(int j = 0; j <= query[i]; j++)
+            {
+                temp.push_back(arr[j]);
+            }
+        }
+        else
+        {   // 홀수
+            for(int j = query[i]; j < arr.size(); j++)
+            {
+                temp.push_back(arr[j]);
+            }
+        }
+        
+        arr = temp;
+    }
+    
+    return arr;
+}
