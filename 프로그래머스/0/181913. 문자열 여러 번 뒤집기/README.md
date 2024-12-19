339c3aab2f07045e0108ebd3e12456f3732618de
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2024-12-19 14:22:01 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.17 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/README.md
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 여러 번 뒤집기 - 181913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181913) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181913?language=csharp) 
 
 ### 성능 요약
 
-메모리: 34.7 MB, 시간: 46.69 ms
+메모리: 31.2 MB, 시간: 1.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 19일 14:04:44
+2024년 12월 19일 14:21:59
 
 ### 문제 설명
 
--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cs
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cs
@@ -2,19 +2,9 @@ using System;
 
 public class Solution 
 {
-    private string ReverseString(string targetStr)
-    {
-        string answer = string.Empty;
-        for (int i = targetStr.Length - 1; i >= 0; i--)
-        {
-            answer += targetStr[i];
-        }
-        return answer;
-    }
-
     public string solution(string my_string, int[,] queries)
     {
-        var answer = my_string;
+        var answer = my_string.ToCharArray();
 
         for (int i = 0; i < queries.GetLength(0); i++)
         {
@@ -22,11 +12,9 @@ public class Solution
             var endIndex = queries[i, 1];
 
             int targetLength = endIndex - startIndex + 1;
-            var subStr = answer.Substring(startIndex, targetLength);
-            answer = answer.Remove(startIndex, targetLength);
-            answer = answer.Insert(startIndex, ReverseString(subStr));
+            Array.Reverse(answer, startIndex, targetLength);
         }
 
-        return answer;
+        return new string(answer);
     }
 }
