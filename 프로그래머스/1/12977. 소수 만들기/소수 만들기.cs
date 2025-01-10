8dbe16f06a74d7eb996613c61c17fa3f76c8927f
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2025-01-10 16:35:20 +0900

    [level 1] Title: 소수 만들기, Time: 14.20 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
@@ -2,27 +2,30 @@ using System;
 
 class Solution
 {
+    private bool IsPrime(int targetNum)
+    {
+        for(int i = 2; i < targetNum; i++)
+        {
+            if(targetNum % i == 0) return false;
+        }
+        
+        return true;
+    }
+    
     public int solution(int[] nums)
     {
         int answer = 0;
+        
         for(int i = 0; i < nums.Length; i++)
         {
             for(int j = i + 1; j < nums.Length; j++)
             {
                 for(int k = j + 1; k < nums.Length; k++)
                 {
-                    var sumNum = nums[i] + nums[j] + nums[k];
-                    int count = 0;
-                    for(int h = 2; h < sumNum; h++)
+                    if(IsPrime(nums[i] + nums[j] + nums[k]))
                     {
-                        if(sumNum % h == 0)
-                        {
-                            count++;
-                            break;
-                        }
+                        answer++;
                     }
-                    
-                    if(count == 0) answer++;
                 }
             }
         }
