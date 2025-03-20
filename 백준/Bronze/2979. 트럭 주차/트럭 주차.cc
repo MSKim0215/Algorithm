ab869bb0d75673c140a4f388f86b0f986b0adcdc
Kyatto-dev <64528545+MSKim0215@users.noreply.github.com>
2025-03-20 17:25:47 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -7,28 +7,26 @@ int main()
     int A, B, C;
     cin >> A >> B >> C;
     
-    vector<int> timeVec[3];
+    int timeCheck[100] { 0, };
     for(int i = 0; i < 3; i++)
     {
         int input, output;
         cin >> input >> output;
         
-        timeVec[i].push_back(input);
-        timeVec[i].push_back(output);
+        for(int j = input; j < output; j++)
+        {
+            timeCheck[j - 1]++;
+        }
     }
     
     int totalSum = 0;
-    for(int i = 1; i <= 100; i++)
+    for(int i = 0; i < 100; i++)
     {
-        int count = 0;
+        if(timeCheck[i] == 0) continue;
         
-        if(timeVec[0][0] <= i && timeVec[0][1] > i) count++;   
-        if(timeVec[1][0] <= i && timeVec[1][1] > i) count++;
-        if(timeVec[2][0] <= i && timeVec[2][1] > i) count++;
-        
-        if(count == 1)  totalSum += (A * count);
-        else if(count == 2) totalSum += (B * count);
-        else if(count == 3) totalSum += (C * count);
+        if(timeCheck[i] == 1) totalSum += (timeCheck[i] * A);
+        else if(timeCheck[i] == 2) totalSum += (timeCheck[i] * B);
+        else if(timeCheck[i] == 3) totalSum += (timeCheck[i] * C);
     }
     
     cout << totalSum << endl;
