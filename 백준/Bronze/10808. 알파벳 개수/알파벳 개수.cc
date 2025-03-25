7ad04096b1d7d55efa25a4887b45f0a6307e4ae0
Kyatto-dev <64528545+MSKim0215@users.noreply.github.com>
2025-03-25 18:43:56 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -7,19 +7,19 @@ int main()
     string answer;
     cin >> answer;
     
-    map<char, int> alphabetMap;
+    map<char, int> alphabetsMap;
     for(int i = 0; i < 26; i++)
     {
-        alphabetMap.insert(pair<char, int>((char)(i + 97), 0));
+        alphabetsMap[i + 97] = 0;
     }
     
     for(int i = 0; i < answer.length(); i++)
     {
-        alphabetMap[answer[i]]++;
+        alphabetsMap[answer[i]]++;
     }
     
-    for(auto it = alphabetMap.begin(); it != alphabetMap.end(); it++)
+    for(auto iter = alphabetsMap.begin(); iter != alphabetsMap.end(); iter++)
     {
-        cout << it->second << " ";
+        cout << iter->second << " ";
     }
 }
